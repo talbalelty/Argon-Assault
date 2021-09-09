27e0b89ef6422489153348ab5c2dd15ffad9d107
Typhonnn <46194035+Typhonnn@users.noreply.github.com>
2021-09-09 17:58:52 +0300

    Added Enemies with wave animations (duplicates), score is given on kill, master timeline now include slackbar voice lines

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,16 +6,19 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scoreAmount = 10;
     [SerializeField] int health = 3;
 
+    GameObject parentGameObject;
     ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     // Update is called once per frame
@@ -26,8 +29,6 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
-        scoreBoard.UpdateScore(scoreAmount);
-        health--;
         ProcessHit(other);
         if (health < 1)
         {
@@ -38,14 +39,16 @@ public class Enemy : MonoBehaviour
     void ProcessHit(GameObject other)
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent; // organize all explosions as children
+        vfx.transform.parent = parentGameObject.transform; // organize all explosions as children
+        health--;
     }
 
     // ParticleSystem explosion - destroys itself automatically
     void ProcessKill()
     {
+        scoreBoard.UpdateScore(scoreAmount);
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent; // organize all explosions as children
+        vfx.transform.parent = parentGameObject.transform; // organize all explosions as children
         Destroy(gameObject);
     }
 }
